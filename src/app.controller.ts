c705044e910595a2751893d1a04c6cb6d6c85828
Lân Bùi Tấn <tanlanhcmus@gmail.com>
2021-09-16 12:50:35 +0700

    feat(): add view

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @Render('login')
+  index() {
+    return;
+  }
+
+  @Post('/login')
+  login(@Res() res: Response): void {
+    res.redirect('/home');
+  }
+
+  @Get('/home')
+  @Render('home')
+  getHome() {
+    return;
+  }
+
+  @Get('/profile')
+  @Render('profile')
+  getProfile() {
+    return;
+  }
+
+  @Get('/logout')
+  logout(@Res() res: Response): void {
+    res.redirect('/');
   }
 }
